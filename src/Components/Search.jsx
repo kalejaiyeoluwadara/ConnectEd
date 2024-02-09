d5c612bb4b131e2067f409a36251452b504c15c9
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-02-09 21:45:11 +0100

    home page

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import add from "../assets/images/add.png";
+import { BsSearch } from "react-icons/bs";
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="w-full flex justify-between items-center px-8 py-2 ">
+      <div className="bg-gray-700 text-white px-2 flex gap-2 items-center h-[50px] w-[80%] sm:w-[300px] rounded-[8px] ">
+        <BsSearch size={20} />
+        <input
+          className="w-[80%] h-full outline-none  bg-transparent"
+          placeholder="search"
+          type="text"
+          name=""
+          id=""
+        />
+      </div>
+      <img src={add} alt="" />
+    </div>
+  );
 }
 
 export default Search;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Components/Header";
+import Search from "../Components/Search";
 
 function Home() {
   return (
     <div className="h-screen w-screen bg-black flex flex-col ">
       <Header />
+      <Search />
     </div>
   );
 }
