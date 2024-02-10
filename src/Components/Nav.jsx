805b2096d9db1e0b039db127a9e29b876b75a568
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-02-10 22:45:12 +0100

    error

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { BsHouse, BsSearch, BsBell, BsPlus } from "react-icons/bs";
+import { BsHouse, BsHouseFill, BsSearch, BsBell, BsPlus } from "react-icons/bs";
 import face1 from "../assets/images/face2.png";
+import { CiBookmark } from "react-icons/ci";
 import { useGlobal } from "../context";
+import { IoBookmark } from "react-icons/io5";
 function Nav() {
-  const { setPage } = useGlobal();
+  const { setPage, home, bookmark } = useGlobal();
   return (
     <footer className="w-screen cursor-pointer flex  items-center px-4 justify-between bg-gray-800 z-50 fixed bottom-0 h-[75px] rounded-t-[20px] left-0">
       <div
@@ -11,10 +13,10 @@ function Nav() {
           setPage("home");
         }}
       >
-        <BsHouse size={25} />
+        {home ? <BsHouseFill size={25} /> : <BsHouse size={25} />}
       </div>
       <div>
-        <BsSearch size={25} />
+        {bookmark ? <IoBookmark size={25} /> : <CiBookmark size={25} />}
       </div>
       <div className="h-[60px] w-[60px] flex items-center justify-center bg-blue-600 rounded-[50%] ">
         <BsPlus size={45} />
