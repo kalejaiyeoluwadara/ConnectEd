11bd9dfccd4d5fc94704722183b25e8b50cf46ef
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-02-14 00:57:30 +0100

    done for tonight

--- src/Components/Listings.jsx
+++ src/Components/Listings.jsx
@@ -30,17 +30,22 @@ function Listings() {
       {courses.map((course) => (
         <div
           key={course.id}
-          className="h-[300px] rounded-[8px] relative w-[90%] sm:w-[300px] bg-gray-800 py-12"
+          className="h-[300px] relative rounded-[8px]  w-[90%] sm:w-[300px] bg-gray-800 py-12"
         >
           {course.isFree && (
             <div className="absolute left-3 top-3 bg-white px-3 py-[3px] rounded-[5px] text-black font-bold">
               Free
             </div>
           )}
+          <img
+            className=" h-full absolute top-0 left-0 w-full "
+            src={course.image}
+            alt="course_images"
+          />
           <div className="absolute right-3 top-3 px-3 py-[3px] rounded-[5px] font-bold">
             <CiBookmark size={25} />
           </div>
-          <div className="flex items-center bg-blue-400 bg-opacity-40 w-full px-2 justify-start absolute bottom-0 rounded-b-[8px] left-0 py-4 gap-1">
+          <div className="flex items-center bg-blue-400 bg-opacity-[0.1] w-full px-2 justify-start absolute bottom-0 rounded-b-[8px] left-0 py-4 gap-1">
             <section className="px-2">
               <h4 className="font-bold text-[20px] ">{course.title}</h4>
               <p>{course.description}</p>
