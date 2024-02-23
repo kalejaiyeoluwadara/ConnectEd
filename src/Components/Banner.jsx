36ad435b4a304680aa158106df3824d066f4238b
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-02-23 22:25:24 +0100

    working

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,26 +1,59 @@
 import React,{useState,useEffect} from 'react'
-
+import { useGlobal } from '../context';
+import { motion,AnimatePresence } from 'framer-motion';
 function Banner() {
-    const [modal,setModal] = useState(true)
+    const [modal,setModal] = useState(true);
+    const {setPage} = useGlobal();
+    
   return (
-    <>
-      modal && {
-        <div className="w-full z-50  fixed top-0 left-0 flex items-center justify-center ">
+    <AnimatePresence>
+      {modal && (
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: "-20vh",
+          }}
+          transtion={{
+            delay:0.5,
+            duration:0.5
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          exit={{
+            opacity: 0,
+            y: "-20vh",
+          }}
+          className="w-full z-50  fixed top-0 left-0 flex items-center justify-center "
+        >
           <div className="flex flex-col rounded-[12px] px-3 mt-3 bg-blue-400 gap-2 py-2 ">
-            <p className="text-end text-[20px] ">x</p>
-            <p className="text-[16px] w-[300px] ">
+            <p
+              onClick={() => {
+                setModal(!modal);
+              }}
+              className="text-end transition-all cursor-pointer hover:text-red-400 text-[20px] "
+            >
+              x
+            </p>
+            <p className="text-[16px]  w-[300px] ">
               Edit your profile, let others know more about you, include your
               social accounts and numbers.
             </p>
             <section className="flex justify-center">
-              <button className="px-4 py-1 border border-white rounded-[16px]  ">
+              <button
+                onClick={() => {
+                  setPage("edit");
+                }}
+                className="px-4  py-1 border border-white rounded-[16px]  "
+              >
                 Edit Profile
               </button>
             </section>
           </div>
-        </div>
-      }
-    </>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
 
