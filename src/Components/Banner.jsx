fbdaadcc4639497c1dc478dd1bca14da4ce5fb9d
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-02-23 22:13:55 +0100

    updates

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='w-screen  fixed top-0 left-0 flex items-center justify-center '>
-      <div className='flex flex-col px-3 bg-blue-400 py-2 '>
+    <div className='w-full z-50  fixed top-0 left-0 flex items-center justify-center '>
+      <div className='flex flex-col rounded-[12px] px-3 mt-3 bg-blue-400 gap-3 py-2 '>
         <p className='text-start'>cancel</p>
-        <p>Edit your profile, let others know more about you, include your social accounts and numbers</p>
-        <section className=''>
-        <button className='px-2 py-2 rounded-[16px]  '>Edit Profile</button>
+        <p className='text-[16px] w-[300px] '>Edit your profile, let others know more about you, include your social accounts and numbers.</p>
+        <section className='flex justify-end'>
+        <button className='px-4 py-1 border border-white rounded-[16px]  '>Edit Profile</button>
         </section>
             
       </div>
